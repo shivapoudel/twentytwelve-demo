e07cc1bfe4d64c9aaa17ccbbe0ef0eda21f66ddd
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-02-22 16:44:07 +0545

    SDK

--- functions.php
+++ functions.php
@@ -7,10 +7,42 @@
  * @package Twenty_Twelve_Demo
  */
 
-// Include the main freemius tracker.
-if ( ! function_exists( 'twentytwelve_demo' ) ) {
-	include_once get_stylesheet_directory() . '/tracker.php';
+/**
+ * Create a helper function for easy SDK access.
+ */
+function twentytwelve_demo() {
+	global $twentytwelve_demo;
+
+	if ( ! isset( $twentytwelve_demo ) ) {
+		// Include Freemius SDK.
+		require_once dirname( __FILE__ ) . '/freemius/start.php';
+
+		$twentytwelve_demo = fs_dynamic_init( array(
+			'id'             => '1747',
+			'slug'           => 'twentytwelve-demo',
+			'type'           => 'theme',
+			'public_key'     => 'pk_f2228cc5d6dcab307e2b353521f66',
+			'is_premium'     => false,
+			'has_addons'     => false,
+			'has_paid_plans' => false,
+			'menu'           => array(
+				'override_exact' => true,
+				'first-path'     => 'themes.php',
+				'account'        => false,
+				'contact'        => false,
+				'support'        => false,
+			),
+		) );
+	}
+
+	return $twentytwelve_demo;
 }
+
+// Init Freemius.
+twentytwelve_demo();
+
+// Signal that SDK was initiated.
+do_action( 'twentytwelve_demo_loaded' );
 
 /**
  * Enqueue scripts.
